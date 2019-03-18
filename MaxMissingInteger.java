0998e83a7cfb3053527432b9cf40277375ed03c4
MariamElansary <MariamElansary@users.noreply.github.com>
2019-03-18 12:08:21 +0200

    Update MaxMissingInteger.java

--- MaxMissingInteger.java
+++ MaxMissingInteger.java
@@ -1,26 +1,15 @@
- public static int solution(int[]A, int[]B) {        
-        Arrays.sort(A);
-        Arrays.sort(B);
+ public static int solution(int[]A) {        
+         Arrays.sort(A);     
+         
+         int min = 1; 
+         
+         int N = A.length; 
+
+         for (int i = 0; i < N; i++){
+             if(A[i] == min){
+                 min++;
+             }
+         }   
         
-        int min = 1, max = 1;
-        
-        int n = A.length, m = B.length;
-        
-        for(int i = 0; i < n; i++)
-        {
-            if(min==A[i])
-                min++;
-        }
-        
-        for(int i = 0; i < m; i++)
-        {
-            if(max==B[i])
-                max++;
-        }
-        
-        
-        if (max > min)
-            return max;
-        else
-            return min;
+         return min;   
  }
